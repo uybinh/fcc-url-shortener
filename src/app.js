b2963a2176ce90ff4ec2bd2582db6fda8bfda077
Uy Binh <uybinh@gmail.com>
2018-10-12 21:31:20 +0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+require("./components/database");
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+const app = express();
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const validator = require("validator");
+const ShortenUrlModel = require("./models/shortenurl");
+const Rand = require("./components/random");
+
+const init = function() {
+  app
+    .use(express.static(path.join(__dirname, "public")))
+    .set("views", path.join(__dirname, "views"))
+    .set("view engine", "ejs")
+    .listen(PORT, () => console.log(`App is listening on port ${PORT}`));
+
+  app.get("/", (req, res) => res.render("pages/index"));
+
+  app.post("/api/shorturl/new", urlencodedParser, async (req, res) => {
+    const url = req.body.url;
+    if (!validator.isURL(url)) {
+      res.json({ error: "invalid URL" });
+      return;
+    }
+    const lastUrl = await ShortenUrlModel.getLastUrl();
+    const key = lastUrl ? lastUrl.key + Rand(3) : Rand(3);
+    const shortenUrl = await ShortenUrlModel.createNewUrlAndSave(key, url);
+    res.json({
+      original_url: shortenUrl.url,
+      short_url: shortenUrl.hash
+    });
+  });
+
+  app.get("/api/shorturl/:hash", async (req, res) => {
+    const hash = req.params.hash;
+    const { url } = await ShortenUrlModel.getUrlByHash(hash);
+    const redirectUrl = url.indexOf("http") === -1 ? `http://${url}` : url;
+    res.redirect(redirectUrl);
+  });
+
+  app.get("/api/listallurls", async (req, res) => {
+    res.json(await ShortenUrlModel.findAllUrls());
+  });
+
+  // respond status 404 with 'Not found' on other paths weren't catched by above routes
+  app.use((req, res, next) => {
+    res
+      .status(404)
+      .type("text")
+      .send("Not found");
+  });
+};
+
+module.exports = init;
---
+++ test.js
@@ -0,0 +1,8 @@
+const Rand = require("./src/components/random");
+const test = require("tape");
+
+test("random number with n digits", function(t) {
+  t.plan(2);
+  t.equal(Rand(2) < 100, true);
+  t.equal(Rand(3) < 1000, true);
+});
